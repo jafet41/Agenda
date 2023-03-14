3edff7ab11401a577bd44454eb52a8dfb8cab0f6
jafet41 <jafet_41@hotmail.com>
2023-03-13 19:22:54 -0600

    Add files via upload

---
+++ build/precache-manifest.f05575f453c6a6373be672aa96b1a84b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "111fa56e1d9a45c021c7",
+    "url": "/static/css/main.54be0fa7.chunk.css"
+  },
+  {
+    "revision": "111fa56e1d9a45c021c7",
+    "url": "/static/js/main.111fa56e.chunk.js"
+  },
+  {
+    "revision": "77ab63ff850dc423d02a",
+    "url": "/static/js/1.77ab63ff.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67ec2ad986e28ad815feeaefc5f8b6fd",
+    "url": "/index.html"
+  }
+];
